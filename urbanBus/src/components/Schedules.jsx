c8387c455e036c930a0ade539f9070c4577a77b9
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-04-26 00:22:12 +0100

    Add first and last stops id's again

--- urbanBus/src/components/Schedules.jsx
+++ urbanBus/src/components/Schedules.jsx
@@ -168,12 +168,12 @@ export default function Schedules() {
 			<div id="menu1" className="flex-1 transition-all h-1">
 				<div className="flex flex-col h-full gap-2"> {/* Gap between input and list */}
 					<div id="stopInputs" className="flex-0 flex flex-col gap-2"> {/* Gap between the 2 inputs */}
-						<label className="input input-bordered flex items-center gap-2">
+						<label id="firstStop" className="input input-bordered flex items-center gap-2">
 							<input type="text" className="grow" onFocus={handleFocus} onBlur={handleBlur} onChange={handleChange} placeholder="Search for a stop" value={startStop} />
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
 						</label>
 
-						<label className="input input-bordered flex items-center gap-2">
+						<label id="lastStop" className="input input-bordered flex items-center gap-2">
 							<input type="text" className="grow" onFocus={handleFocus} onBlur={handleBlur} onChange={handleChange} placeholder="Search your destiny" value={lastStop} />
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
 						</label>
