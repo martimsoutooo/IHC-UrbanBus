113719ec6cd1a94c714b2e5f1de36143fe79f414
martimsantos <santos.martim@ua.pt>
2024-05-07 15:39:51 +0100

    pwa

--- urbanBus/astro.config.mjs
+++ urbanBus/astro.config.mjs
@@ -22,17 +22,17 @@ export default defineConfig({
 				display: "fullscreen",
 				icons: [
 					{
-						src: 'pwa-192x192.png',
+						src: 'appIcon192x192.png',
 						sizes: '192x192',
 						type: 'image/png',
 					},
 					{
-						src: 'pwa-512x512.png',
+						src: 'appIcon512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 					},
 					{
-						src: 'pwa-512x512.png',
+						src: 'appIcon512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'any maskable',
