b6f189c27ff422845494ff6b334f77e8d338c58f
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-05-11 18:05:20 +0100

    Add /app scope to pwa.js

--- urbanBus/src/scripts/pwa.js
+++ urbanBus/src/scripts/pwa.js
@@ -2,9 +2,11 @@ import { registerSW } from 'virtual:pwa-register'
 
 registerSW({
   immediate: true,
+  scope: '/app',
   onRegisteredSW(swScriptUrl) {
     // eslint-disable-next-line no-console
     console.log('SW registered: ', swScriptUrl)
+	
   },
   onOfflineReady() {
     // eslint-disable-next-line no-console
