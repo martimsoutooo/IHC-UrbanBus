7c66a59ed47c9a9d97daf59a8c2b01f5607725e3
GabrielSilva <gabrielmsilva4@ua.pt>
2024-05-11 04:29:17 +0100

    add bar with back button in Stoplist

--- urbanBus/src/components/StopListResult.jsx
+++ urbanBus/src/components/StopListResult.jsx
@@ -80,20 +80,31 @@ export default function StopListResult(props) {
 	}
 
 	return (
-		<ul className="w-full p-0">
-			{(journeys).sort((a, b) => {
-				return a.startTime > b.startTime ? 1 : -1;
-			}).map((journey) => {
-				if (journey.line === line) {
-					return (
-						<li key={journey.line + journey.startTime} onClick={() => {handleClick(journey.line)}} onMouseDown={() => {handleFocus(journey.line)}}>
-							<a href='/app/tripTimeline'>
-								<JourneyCard line={journey.line} startTime={journey.startTime} endTime={journey.endTime} />
-							</a>
-						</li>
-					);
-				}
-			})}
-		</ul>
+		<div>
+			<div className="flex flex-row items-center gap-4">
+                <button className="btn btn-circle btn-ghost">
+                    <a href='/app/schedules'>
+                        <svg className='h-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+                    </a>
+                </button>
+                <p className='text-2xl font-bold'>{line} - Trips</p>
+            </div>
+
+			<ul className="w-full p-0">
+				{(journeys).sort((a, b) => {
+					return a.startTime > b.startTime ? 1 : -1;
+				}).map((journey) => {
+					if (journey.line === line) {
+						return (
+							<li key={journey.line + journey.startTime} onClick={() => {handleClick(journey.line)}} onMouseDown={() => {handleFocus(journey.line)}}>
+								<a href='/app/tripTimeline'>
+									<JourneyCard line={journey.line} startTime={journey.startTime} endTime={journey.endTime} />
+								</a>
+							</li>
+						);
+					}
+				})}
+			</ul>
+		</div>
 	);
 }
--- urbanBus/src/components/Timeline.jsx
+++ urbanBus/src/components/Timeline.jsx
@@ -13,7 +13,7 @@ export default function Timeline() {
                         <svg className='h-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
                     </a>
                 </button>
-                <p className='text-2xl font-bold'>Horário - Linha 1</p>
+                <p className='text-2xl font-bold'>Schedule - Line 1</p>
             </div>
 
             <div className="divider my-0"></div>
