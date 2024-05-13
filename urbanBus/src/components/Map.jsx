f8de06a4e31346a30c31f86bf92ff1d6e433bda2
GabrielSilva <gabrielmsilva4@ua.pt>
2024-05-13 23:32:32 +0100

    import stops

--- urbanBus/src/components/Map.jsx
+++ urbanBus/src/components/Map.jsx
@@ -1,51 +1,8 @@
 import React, { useEffect } from 'react';
 import L from 'leaflet';
+import { stops } from './consts/stops.js';
 
 function Map() {
-	const stops = [
-		{
-			"id": 1,
-			"latitude": "-8.641420",
-			"location": "R. Cmte. Rocha e Cunha 156, 3800-179 Aveiro",
-			"longitude": "40.643450",
-			"name": "Esta\u00e7\u00e3o de Aveiro"
-		},
-		{
-			"id": 2,
-			"latitude": "-8.645047",
-			"location": "Av. Dr. Louren\u00e7o Peixinho 216A, 3800-181 Aveiro",
-			"longitude": "40.643250",
-			"name": "Av. Dr. Louren\u00e7o Peixinho - CTT B"
-		},
-		{
-			"id": 3,
-			"latitude": "-8.653030",
-			"location": "R. do Batalh\u00e3o de Ca\u00e7adores 22-26, 3810-064 Aveiro",
-			"longitude": "40.640790",
-			"name": "Ca\u00e7adores 10 - Miseric\u00f3rdia"
-		},
-		{
-			"id": 4,
-			"latitude": "-8.656961",
-			"location": "Antiga Reitoria, 3810-193 Aveiro",
-			"longitude": "40.634953",
-			"name": "Universidade - Antiga Reitoria A"
-		},
-		{
-			"id": 5,
-			"latitude": "-8.656840",
-			"location": "Antiga Reitoria, 3810-193 Aveiro",
-			"longitude": "40.634900",
-			"name": "Universidade - Antiga Reitoria B"
-		},
-		{
-			"id": 6,
-			"latitude": "-8.659240",
-			"location": "Aradas, 3810-193",
-			"longitude": "40.623050",
-			"name": "Universidade Crasto"
-		}
-	]
 	useEffect(() => {
 		const map = L.map('map',{
 			attributionControl: false,
