630192a2a96a50c3da5c6ae49b85b3ede507b099
GabrielSilva <gabrielmsilva4@ua.pt>
2024-05-13 23:32:54 +0100

    add some random journeys to a file

---
+++ urbanBus/src/components/consts/journeys.js
@@ -0,0 +1,16 @@
+const journeys = [
+    {"line": "L1", "firstStop": {"name": "Hospital / Universidade B"}, "lastStop": {"name": "Escola de Eirol A"}, "startTime": "7:00", "endTime": "7:30"},
+    {"line": "L1", "firstStop": {"name": "Escola de Eirol A"}, "lastStop": {"name": "Capela da Taipa A"}, "startTime": "7:00", "endTime": "8:00"},
+    {"line": "L2", "firstStop": {"name": "Azurva (Pontão A17) B"}, "lastStop": {"name": "Carregal - Areosa A"}, "startTime": "7:00", "endTime": "7:00", "delay": 7},
+    {"line": "L2", "firstStop": {"name": "Carregal - Areosa A"}, "lastStop": {"name": "R. Solposto B"}, "startTime": "7:00", "endTime": "8:00", "delay": 5},
+    {"line": "L3", "firstStop": {"name": "Av. Dr. Lourenço Peixinho - CTT B"}, "lastStop": {"name": "Carregal - Areosa A"}, "startTime": "7:00", "endTime": "7:00", "delay": 3},
+    {"line": "L3", "firstStop": {"name": "Carregal - Areosa A"}, "lastStop": {"name": "Azurva (Pontão A17) B"}, "startTime": "7:00", "endTime": "8:00"},
+    {"line": "L4", "firstStop": {"name": "Colégio D. José I"}, "lastStop": {"name": "Escola das Leirinhas"}, "startTime": "7:00", "endTime": "7:00"},
+    {"line": "L4", "firstStop": {"name": "Escola das Leirinhas"}, "lastStop": {"name": "Caçadores 10 - Sé"}, "startTime": "8:00", "endTime": "8:40"},
+    {"line": "L1", "firstStop": {"name": "Estação de Aveiro"}, "lastStop": {"name": "R. Luis G. Carvalho"}, "startTime": "7:00", "endTime": "9:00"},
+    {"line": "L1", "firstStop": {"name": "R. Luis G. Carvalho"}, "lastStop": {"name": "Av. Dr. Lourenço Peixinho - CTT B"}, "startTime": "7:00", "endTime": "8:00"},
+    {"line": "L2", "firstStop": {"name": "Av. Dr. Lourenço Peixinho - Capitania B"}, "lastStop": {"name": "Caçadores 10 - Misericórdia"}, "startTime": "7:00", "endTime": "7:00", "delay": 9},
+    {"line": "L2", "firstStop": {"name": "Caçadores 10 - Misericórdia"}, "lastStop": {"name": "Caçadores 10 - Sé"}, "startTime": "7:00", "endTime": "8:00"},
+    {"line": "L3", "firstStop": {"name": "Universidade - Antiga Reitoria A"}, "lastStop": {"name": "Universidade - Antiga Reitoria B"}, "startTime": "7:00", "endTime": "7:00"},
+    {"line": "L3", "firstStop": {"name": "Universidade - Antiga Reitoria B"}, "lastStop": {"name": "Hospital / Universidade A"}, "startTime": "7:00", "endTime": "8:00", "delay": 9}
+]
