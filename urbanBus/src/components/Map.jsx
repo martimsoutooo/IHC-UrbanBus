db9c4c313166fc0694b727afd0c20bd8c0d2e758
GabrielSilva <gabrielmsilva4@ua.pt>
2024-05-14 22:42:59 +0100

    add prop line to timeline

--- urbanBus/src/components/Map.jsx
+++ urbanBus/src/components/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import L from 'leaflet';
-import { stops } from './consts/stops.js';
+import { _stops } from './consts/stops.js';
 
 function Map() {
 	useEffect(() => {
@@ -23,8 +23,8 @@ function Map() {
 		}).addTo(map);
 
 		// Add markers to the map
-		for (let i = 0; i < stops.length; i++) {
-			const stop = stops[i];
+		for (let i = 0; i < _stops.length; i++) {
+			const stop = _stops[i];
 			L.marker([stop.longitude, stop.latitude]).addTo(map).bindPopup(stop.name + '<div class="flex justify-center"><a href="/app?search=' + stop.name + '"><button class="btn btn-sm btn-neutral text-white mt-3">Go to</button></div>');
 		}
 
