1257ecc00378d18054dc1bc7f6196f22c4b5428d
GabrielSilva <gabrielmsilva4@ua.pt>
2024-05-27 04:18:39 +0100

    fix pages

--- urbanBus/src/components/AdminWeb/SubPageLeft.jsx
+++ urbanBus/src/components/AdminWeb/SubPageLeft.jsx
@@ -10,15 +10,12 @@ export default function SubPageLeft({ currentPage, setCurrentPage }) {
     
     return (
         <div className="w-full ml-8 mr-8 mt-6">
-            <StopsPage />
-            {/*
             {currentPage === 'Home' && <HomePage setCurrentPage={setCurrentPage} />}
             {currentPage === 'Stops' && <StopsPage />}
             {currentPage === 'Lines' && <LinesPage />}
             {currentPage === 'Tickets' && <TicketsPage />}
             {currentPage === 'Clients' && <ClientsPage />}
             {currentPage === 'Validations' && <ValidationsPage />}
-    */}
         </div>
     )
 }
