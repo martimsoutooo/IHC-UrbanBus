1208f67c10802e0dccd22b6df841606ff209031b
GabrielSilva <gabrielmsilva4@ua.pt>
2024-05-30 23:16:55 +0100

    add missing icons

--- urbanBus/src/components/AdminWeb/HomePage.jsx
+++ urbanBus/src/components/AdminWeb/HomePage.jsx
@@ -66,18 +66,26 @@ export default function HomePage({setCurrentPage}) {
 
                 <button className="card card-bordered w-96 bg-base-100 shadow-xl"
                         onClick={() => setCurrentPage('Validations')}>
-                    <div className="card-body">
-                        <h2 className="card-title">Validations</h2>
-                        <p></p>
+                    <div className="card-body flex flex-row gap-7">
+                        <svg className="size-16 my-auto mx-1" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 512 512">
+                            <path fill='#333c4d'
+                                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
+                        </svg>
+                        <div className="my-auto">
+                            <h1 className="card-title text-4xl">Validations</h1>
+                            <p></p>
+                        </div>
                     </div>
                 </button>
 
                 <button className="card card-bordered w-96 bg-base-100 shadow-xl"
                         onClick={() => setCurrentPage('Journeys')}>
                     <div className="card-body flex flex-row gap-7">
-                        <svg className="size-16 my-auto mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                        <svg className="size-16 my-auto mx-1" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 576 512">
                             <path fill='#333c4d'
-                                  d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/>
+                                  d="M288 0C422.4 0 512 35.2 512 80V96l0 32c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32l0 160c0 17.7-14.3 32-32 32v32c0 17.7-14.3 32-32 32H416c-17.7 0-32-14.3-32-32V448H192v32c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32l0-32c-17.7 0-32-14.3-32-32l0-160c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h0V96h0V80C64 35.2 153.6 0 288 0zM128 160v96c0 17.7 14.3 32 32 32H272V128H160c-17.7 0-32 14.3-32 32zM304 288H416c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H304V288zM144 400a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm288 0a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM384 80c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16s7.2 16 16 16H368c8.8 0 16-7.2 16-16z"/>
                         </svg>
                         <div className="my-auto">
                             <h1 className="card-title text-4xl">Journeys</h1>
